d16b78904e6e273549ce0f3fb5a0a8992b8cebb6
Nathan Frost <nfrost@jamasoftware.com>
2017-08-01 23:38:28 -0700

    add some state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import React, { Component } from 'react';
 import schedule from './masterschedule.json';
 import { Row, Col } from 'react-bootstrap';
 import { Event } from './components/Event';
+import uuid from 'uuid/v4';
 
 class App extends Component {
+    getInitialState(schedule) {
+        return schedule.map(event => {
+            event.uuid = uuid()
+            return event
+        })
+    }
+    constructor(props) {
+        super(props)
+        const newSchedule = this.getInitialState(schedule)
+        this.state = { schedule: newSchedule }
+    }
+
     render() {
+        const { schedule } = this.state
         return (
             <div>
                 <div>
                     Lil' Sched
-                    <p>
-                        Thursday Events:
-                    </p>
                 </div>
-                {schedule
-                    .filter(details => details.day === 'Thursday')
-                    .map(details => Event(details))}
+                {schedule.map(details => Event(details))}
             </div>
-        );
+        )
     }
 }
 
