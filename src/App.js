67ffd1cde4ab3bcbb7d6afb3aaedec459cc4c0d5
Nathan Frost <nfrost@jamasoftware.com>
2017-08-02 20:56:04 -0700

    adding stage filter behavior

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import { Row, Col } from 'react-bootstrap';
 import { Event } from './components/Event';
 import uuid from 'uuid/v4';
 import { PageHeader } from './components/PageHeader';
+import {
+    GALAXY_BARN,
+    LUCKY_BARN,
+    MT_HOOD_STAGE,
+    STARLIGHT_STAGE,
+    TREELINE_STAGE,
+    WOOD_STAGE,
+} from './constants'
+
+function mapLocationToStageId(location) {
+    switch(location) {
+        case 'Treeline Stage': return TREELINE_STAGE
+        case 'Mt. Hood Stage': return MT_HOOD_STAGE
+        case 'Galaxy Barn': return GALAXY_BARN
+        case 'Starlight Stage': return STARLIGHT_STAGE
+        case 'Lucky Barn': return LUCKY_BARN
+        case 'Woods Stage': return WOOD_STAGE
+        default: return undefined
+    }
+}
 
 class App extends Component {
     constructor(props) {
@@ -16,10 +36,19 @@ class App extends Component {
             event.uuid = id
             event.isSelected = false
             event.onClick = this.onEventClick.bind(this, id)
+            event.stageId = mapLocationToStageId(event.location)
             eventDictionary[id] = event
         })
+        const stageFilter = {
+            GALAXY_BARN: true,
+            LUCKY_BARN: true,
+            MT_HOOD_STAGE: true,
+            STARLIGHT_STAGE: true,
+            TREELINE_STAGE: true,
+            WOOD_STAGE: true,
+        }
 
-        this.state = { eventDictionary }
+        this.state = { eventDictionary, stageFilter }
     }
 
     onEventClick(uuid) {
@@ -28,6 +57,17 @@ class App extends Component {
             prevState.eventDictionary[uuid].isSelected = !isSelected
             return prevState
         })
+    }
+
+    onStageFilterClick(stage) {
+        this.setState(prevState => {
+            const isToggled = prevState.stageFilter[stage]
+            prevState.stageFilter[stage] = !isToggled
+        })
+    }
+
+    filterStages(stageId) {
+        return this.state.stageFilter[stageId]
     }
 
     toEventArray(eventDictionary) {
@@ -40,11 +80,13 @@ class App extends Component {
 
         return (
             <div>
-                <PageHeader />
+                <PageHeader onStageFilterClick={this.onStageFilterClick.bind(this)} />
                 <Row style={pageBody}>
                     <Col sm={0} md={3}></Col>
                     <Col sm={12} md={6}>
-                        {eventArray.map(details => Event(details))}
+                        {eventArray
+                            .filter(event => this.filterStages(event.stageId))
+                            .map(details => Event(details))}
                     </Col>
                     <Col sm={0} md={3}></Col>
                 </Row>
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Navbar, Row, Col, Button, FormGroup, FormControl } from 'react-bootstrap';
 import {
+    GALAXY_BARN_COLOR,
+    LUCKY_BARN_COLOR,
+    MT_HOOD_STAGE_COLOR,
+    STARLIGHT_STAGE_COLOR,
+    TREELINE_STAGE_COLOR,
+    WOOD_STAGE_COLOR,
     GALAXY_BARN,
     LUCKY_BARN,
     MT_HOOD_STAGE,
     STARLIGHT_STAGE,
     TREELINE_STAGE,
     WOOD_STAGE,
-} from '../colorConstants'
+} from '../constants'
 
 export function PageHeader(props) {
     const {
@@ -20,7 +26,9 @@ export function PageHeader(props) {
         treelineStyle,
         woodStyle,
     } = styles
-    
+
+    const { onStageFilterClick } = props
+
     return (<Navbar fixedTop>
         <Navbar.Header>
             <Navbar.Collapse>
@@ -31,12 +39,18 @@ export function PageHeader(props) {
         </Navbar.Header>
         <Navbar.Form>
             <FormGroup style={buttonBarStyle}>
-                <Button style={{...galaxyStyle, ...button}}></Button>
-                <Button style={{...luckyStyle, ...button}}></Button>
-                <Button style={{...mtHoodStyle, ...button}}></Button>
-                <Button style={{...starlightStyle, ...button}}></Button>
-                <Button style={{...treelineStyle, ...button}}></Button>
-                <Button style={{...woodStyle, ...button}}></Button>
+                <Button style={{...galaxyStyle, ...button}}
+                    onClick={() => onStageFilterClick(GALAXY_BARN)} />
+                <Button style={{...luckyStyle, ...button}}
+                    onClick={() => onStageFilterClick(LUCKY_BARN)} />
+                <Button style={{...mtHoodStyle, ...button}}
+                    onClick={() => onStageFilterClick(MT_HOOD_STAGE)} />
+                <Button style={{...starlightStyle, ...button}}
+                    onClick={() => onStageFilterClick(STARLIGHT_STAGE)} />
+                <Button style={{...treelineStyle, ...button}}
+                    onClick={() => onStageFilterClick(TREELINE_STAGE)} />
+                <Button style={{...woodStyle, ...button}}
+                    onClick={() => onStageFilterClick(WOOD_STAGE)} />
             </FormGroup>
         </Navbar.Form>
     </Navbar>)
@@ -52,24 +66,24 @@ const styles = {
     button: {
         flex: 1,
         padding: '1em',
-        margin: '.2em',        
+        margin: '.2em',
     },
     galaxyStyle: {
-        backgroundColor: GALAXY_BARN,
+        backgroundColor: GALAXY_BARN_COLOR,
     },
     luckyStyle: {
-        backgroundColor: LUCKY_BARN,
+        backgroundColor: LUCKY_BARN_COLOR,
     },
     mtHoodStyle: {
-        backgroundColor: MT_HOOD_STAGE,
+        backgroundColor: MT_HOOD_STAGE_COLOR,
     },
     starlightStyle: {
-        backgroundColor: STARLIGHT_STAGE,
+        backgroundColor: STARLIGHT_STAGE_COLOR,
     },
     treelineStyle: {
-        backgroundColor: TREELINE_STAGE,
+        backgroundColor: TREELINE_STAGE_COLOR,
     },
     woodStyle: {
-        backgroundColor: WOOD_STAGE,
+        backgroundColor: WOOD_STAGE_COLOR,
     }
 }
