949e75c767f5514383f6db6e0dd8925f11f78f99
Diana Holland <holla@bikefarm.org>
2020-02-15 15:54:58 -0800

    Improve responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+// import { useSpring, animated } from 'react-spring';
 
 export function Hero(): JSX.Element {
   return (
-    <div>
-      <div className={'hero'} />
-      <div className={'hero-text'}>Shockingly Fast Schedule Viewer</div>
-    </div>
+    <header className={'hero'}>
+      <div className={'hero-content'}>
+        <h1>Pickathon</h1>
+        <h2>Shockingly Fast Schedule Viewer</h2>
+      </div>
+    </header>
   );
 }
