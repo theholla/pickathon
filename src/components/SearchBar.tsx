7237e5c846cf89b90d42ce71bf95d5c4cb3f0536
Diana Holland <holla@bikefarm.org>
2020-02-15 15:57:43 -0800

    Add aria-label

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,7 +7,13 @@ export function SearchBar(props: SearchBarProps): JSX.Element {
   return (
     <form>
       <label className={'search-container'}>
-        <input className={'search-input'} type="text" placeholder="filter by artist" onChange={props.onSearch} />
+        <input
+          aria-label={'Filter by artist'}
+          className={'search-input'}
+          type="text"
+          placeholder="filter by artist"
+          onChange={props.onSearch}
+        />
       </label>
     </form>
   );
